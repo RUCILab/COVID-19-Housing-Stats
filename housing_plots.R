7edcaec41baa8d0cfe2362f62e03c21b39c38c17
Gavin Rozzi <gr@gavinrozzi.com>
2020-08-28 17:22:01 -0400

    Update plots and code

--- housing_plots.R
+++ housing_plots.R
@@ -3,27 +3,27 @@ library(tidyverse)
 library(lubridate)
 library(plotly)
 
-housing <- read_csv('Housing.csv')
+housing <- read_csv('NJHousing.csv')
 
 # Add a day value to X axis so it will plot properly
 housing$month_date_yyyymm <- ymd(paste0(housing$month_date_yyyymm,'01'))
 
+# Subset by year
 housing_2020 <- housing %>% filter(month_date_yyyymm > '2019-12-31')
-
 housing_2019 <- housing %>% filter(month_date_yyyymm < '2020-01-01')
 
 
 # Plot of change in median listing price in dollars from Jan,2020 to present
 
 fig1 <- plot_ly(housing_2020, x = ~month_date_yyyymm) %>% 
-  add_trace(y = ~median_listing_price, name = ~'Median Listing Price',mode = 'lines') %>%
+  add_trace(y = ~median_listing_price, name = 'Median Listing Price',mode = 'lines') %>%
   layout(
     title = "Median Listing Price Changes During COVID-19 in New Jersey",
+    yaxis = title = "Median Listing Price",
     xaxis = list(
       type = "date",
       title = "Month",
       range=c(min(housing_2020$month_date_yyyymm), max(housing_2020$month_date_yyyymm)),
-    yaxis = list(title = "Median Listing Price")
     ))
 fig1
 
@@ -31,11 +31,11 @@ fig1
 # Plot of percent change month-to-month in median listing price from Jan,2020 to present
 
 fig2 <- plot_ly(housing_2020, x = ~month_date_yyyymm) %>% 
-  add_trace(y = ~median_listing_price_yy, name = ~'Change Versus 2019',mode = 'lines') %>%
-  add_trace(y = ~median_listing_price_mm, name = ~'Change Since Month-to-month',mode = 'lines') %>%
+  add_trace(y = ~median_listing_price_yy, name = ~'Change relative to 2019',mode = 'lines') %>%
+  add_trace(y = ~median_listing_price_mm, name = ~'Change month-to-month',mode = 'lines') %>%
   layout(
     title = "Median Listing Price Percent Change During COVID-19 in New Jersey",
-    yaxis = list(title = "Percent Change Relative to 2019",
+    yaxis = list(title = "Percent Change",
                  tickformat = "%"),
     xaxis = list(
       type = "date",
@@ -43,3 +43,54 @@ fig2 <- plot_ly(housing_2020, x = ~month_date_yyyymm) %>%
       range=c(min(housing_2020$month_date_yyyymm), max(housing_2020$month_date_yyyymm))
     ))
 fig2
+
+
+# Plot of active listings percent change 
+
+fig3 <- plot_ly(housing_2020, x = ~month_date_yyyymm) %>% 
+  add_trace(y = ~active_listing_count_yy, name = ~'Change relative to 2019',mode = 'lines') %>%
+  add_trace(y = ~active_listing_count_mm, name = ~'Change month-to-month',mode = 'lines') %>%
+  layout(
+    title = "Percent Change in Active Listings During COVID-19 in New Jersey",
+    yaxis = list(title = "Percent Change",
+                 tickformat = "%"),
+    xaxis = list(
+      type = "date",
+      title = "Month",
+      range=c(min(housing_2020$month_date_yyyymm), max(housing_2020$month_date_yyyymm))
+    ))
+fig3
+
+
+# Plot of active listings percent change 
+
+fig4 <- plot_ly(housing_2020, x = ~month_date_yyyymm) %>% 
+  add_trace(y = ~median_days_on_market_yy, name = ~'Change relative to 2019',mode = 'lines') %>%
+  add_trace(y = ~median_days_on_market_mm, name = ~'Change month-to-month',mode = 'lines') %>%
+  layout(
+    title = "Percent Change in Median Days on the Market During COVID-19 in New Jersey",
+    yaxis = list(title = "Percent Change",
+                 tickformat = "%"),
+    xaxis = list(
+      type = "date",
+      title = "Month",
+      range=c(min(housing_2020$month_date_yyyymm), max(housing_2020$month_date_yyyymm))
+    ))
+fig4
+
+
+# New Listings
+
+fig5 <- plot_ly(housing_2020, x = ~month_date_yyyymm) %>% 
+  add_trace(y = ~new_listing_count_yy, name = ~'Change relative to 2019',mode = 'lines') %>%
+  add_trace(y = ~new_listing_count_mm, name = ~'Change month-to-month',mode = 'lines') %>%
+  layout(
+    title = "Percent Change in New Listings During COVID-19 in New Jersey",
+    yaxis = list(title = "Percent Change",
+                 tickformat = "%"),
+    xaxis = list(
+      type = "date",
+      title = "Month",
+      range=c(min(housing_2020$month_date_yyyymm), max(housing_2020$month_date_yyyymm))
+    ))
+fig5
