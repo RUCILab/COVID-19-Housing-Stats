64e934331b92518c4357d8c5c93fff42cd2c27ac
Gavin Rozzi <gr@gavinrozzi.com>
2020-09-15 10:55:47 -0400

    Update county / regional plots and code

--- county_housing_plots.r
+++ county_housing_plots.r
@@ -1,9 +1,16 @@
+# Plots demonstrating NJ real estate trends by county / region
+
 library(tidyverse)
 library(lubridate)
 library(plotly)
 
 # Read in data and county mapping file
-housing <- read_csv('NJHousing_by_county.csv')
+data <- read_csv('https://econdata.s3-us-west-2.amazonaws.com/Reports/Core/RDC_Inventory_Core_Metrics_County_History.csv')
+
+# Get county-level data for NJ by filtering FIPS code
+housing <- data %>% filter(str_detect(county_fips, "^34"))
+
+# Read in mapping of counties to regions derived from NJ state shapefile
 counties <- read_csv('nj_counties.csv')
 
 # Add a day value to X axis so it will plot properly
@@ -30,42 +37,24 @@ summed <- joined %>% group_by(month_date_yyyymm,REGION) %>%
 
 summed <- ungroup(summed)
 
-# Split summed dataframe by county regions
-
-central <- summed %>% filter(REGION == 'CENTRAL')
-coastal <- summed %>% filter(REGION == 'COASTAL')
-southern <- summed %>% filter(REGION == 'SOUTHERN')
-northeastern <- summed %>% filter(REGION == 'NORTHEASTERN')
-northwestern <- summed %>% filter(REGION == 'NORTHWESTERN')
 
 # Figure 6 - Median List Price Change Relative to 2019
-fig6 <- plot_ly(coastal, x = ~month_date_yyyymm) %>% 
-  add_trace(y = ~median_listing_price_yy, name = 'Coastal', type='scatter', mode = 'lines') %>%
-  add_trace(y = central$median_listing_price_yy, name = 'Central', type='scatter', mode = 'lines') %>%
-  add_trace(y = southern$median_listing_price_yy, name = 'Southern', type='scatter', mode = 'lines') %>%
-  add_trace(y = northeastern$median_listing_price_yy, name = 'Northeastern', type='scatter', mode = 'lines') %>%
-  add_trace(y = northwestern$median_listing_price_yy, name = 'Northwestern', type='scatter', mode = 'lines') %>%
-  
-  layout(
+
+fig6 <- plot_ly(summed, x = ~month_date_yyyymm, y = ~median_listing_price_yy, color = ~REGION, type='scatter', mode = 'lines') %>%
+layout(
     title = "Median Listing Price Change by Region During COVID-19 in New Jersey",
     yaxis = list(title = "Percent Change Since 2019",
                  tickformat = "%"),
     xaxis = list(
       type = "date",
       title = "Month",
-      range=c(min(coastal$month_date_yyyymm), max(coastal$month_date_yyyymm))
+      range=c(min(summed$month_date_yyyymm), max(summed$month_date_yyyymm))
     ))
 fig6
 
 
 # Figure 7 - Median List Price Change Month to Month
-fig7 <- plot_ly(coastal, x = ~month_date_yyyymm) %>% 
-  add_trace(y = ~median_listing_price_mm, name = 'Coastal', type='scatter', mode = 'lines') %>%
-  add_trace(y = central$median_listing_price_mm, name = 'Central', type='scatter', mode = 'lines') %>%
-  add_trace(y = southern$median_listing_price_mm, name = 'Southern', type='scatter', mode = 'lines') %>%
-  add_trace(y = northeastern$median_listing_price_mm, name = 'Northeastern', type='scatter', mode = 'lines') %>%
-  add_trace(y = northwestern$median_listing_price_mm, name = 'Northwestern', type='scatter', mode = 'lines') %>%
-  
+fig7 <- plot_ly(summed, x = ~month_date_yyyymm, y = ~median_listing_price_mm, color = ~REGION, type='scatter', mode = 'lines') %>%
   layout(
     title = "Median Listing Price Change by Region During COVID-19 in New Jersey",
     yaxis = list(title = "Percent Change Since Prior Month",
@@ -73,19 +62,13 @@ fig7 <- plot_ly(coastal, x = ~month_date_yyyymm) %>%
     xaxis = list(
       type = "date",
       title = "Month",
-      range=c(min(coastal$month_date_yyyymm), max(coastal$month_date_yyyymm))
+      range=c(min(summed$month_date_yyyymm), max(summed$month_date_yyyymm))
     ))
 fig7
 
 
 # Figure 8 - Median days on the market relative to 2019
-fig8 <- plot_ly(coastal, x = ~month_date_yyyymm) %>% 
-  add_trace(y = ~median_days_on_market_yy, name = 'Coastal', type='scatter', mode = 'lines') %>%
-  add_trace(y = central$median_days_on_market_yy, name = 'Central', type='scatter', mode = 'lines') %>%
-  add_trace(y = southern$median_days_on_market_yy, name = 'Southern', type='scatter', mode = 'lines') %>%
-  add_trace(y = northeastern$median_days_on_market_yy, name = 'Northeastern', type='scatter', mode = 'lines') %>%
-  add_trace(y = northwestern$median_days_on_market_yy, name = 'Northwestern', type='scatter', mode = 'lines') %>%
-  
+fig8 <- plot_ly(summed, x = ~month_date_yyyymm, y = ~median_days_on_market_yy, color = ~REGION, type='scatter', mode = 'lines') %>%
   layout(
     title = "Median Days on Market by Region During COVID-19 in New Jersey",
     yaxis = list(title = "Percent Change Since 2019",
@@ -93,38 +76,13 @@ fig8 <- plot_ly(coastal, x = ~month_date_yyyymm) %>%
     xaxis = list(
       type = "date",
       title = "Month",
-      range=c(min(coastal$month_date_yyyymm), max(coastal$month_date_yyyymm))
+      range=c(min(summed$month_date_yyyymm), max(summed$month_date_yyyymm))
     ))
 fig8
 
 
-# Figure 8 - Median days on the market relative to 2019
-fig8 <- plot_ly(coastal, x = ~month_date_yyyymm) %>% 
-  add_trace(y = ~median_days_on_market_yy, name = 'Coastal', type='scatter', mode = 'lines') %>%
-  add_trace(y = central$median_days_on_market_yy, name = 'Central', type='scatter', mode = 'lines') %>%
-  add_trace(y = southern$median_days_on_market_yy, name = 'Southern', type='scatter', mode = 'lines') %>%
-  add_trace(y = northeastern$median_days_on_market_yy, name = 'Northeastern', type='scatter', mode = 'lines') %>%
-  add_trace(y = northwestern$median_days_on_market_yy, name = 'Northwestern', type='scatter', mode = 'lines') %>%
-  
-  layout(
-    title = "Median Days on Market by Region During COVID-19 in New Jersey",
-    yaxis = list(title = "Percent Change Since 2019",
-                 tickformat = "%"),
-    xaxis = list(
-      type = "date",
-      title = "Month",
-      range=c(min(coastal$month_date_yyyymm), max(coastal$month_date_yyyymm))
-    ))
-fig8
-
-
-fig9 <- plot_ly(coastal, x = ~month_date_yyyymm) %>% 
-  add_trace(y = ~median_days_on_market_mm, name = 'Coastal', type='scatter', mode = 'lines') %>%
-  add_trace(y = central$median_days_on_market_mm, name = 'Central', type='scatter', mode = 'lines') %>%
-  add_trace(y = southern$median_days_on_market_mm, name = 'Southern', type='scatter', mode = 'lines') %>%
-  add_trace(y = northeastern$median_days_on_market_mm, name = 'Northeastern', type='scatter', mode = 'lines') %>%
-  add_trace(y = northwestern$median_days_on_market_mm, name = 'Northwestern', type='scatter', mode = 'lines') %>%
-  
+# Figure 9 - Median days on the market month-to-month
+fig9 <- plot_ly(summed, x = ~month_date_yyyymm, y = ~median_days_on_market_mm, color = ~REGION, type='scatter', mode = 'lines') %>%
   layout(
     title = "Median Days on Market by Region During COVID-19 in New Jersey",
     yaxis = list(title = "Percent Change Since Prior Month",
@@ -132,17 +90,14 @@ fig9 <- plot_ly(coastal, x = ~month_date_yyyymm) %>%
     xaxis = list(
       type = "date",
       title = "Month",
-      range=c(min(coastal$month_date_yyyymm), max(coastal$month_date_yyyymm))
+      range=c(min(summed$month_date_yyyymm), max(summed$month_date_yyyymm))
     ))
 fig9
 
-fig10 <- plot_ly(coastal, x = ~month_date_yyyymm) %>% 
-  add_trace(y = ~new_listing_count_yy, name = 'Coastal', type='scatter', mode = 'lines') %>%
-  add_trace(y = central$new_listing_count_yy, name = 'Central', type='scatter', mode = 'lines') %>%
-  add_trace(y = southern$new_listing_count_yy, name = 'Southern', type='scatter', mode = 'lines') %>%
-  add_trace(y = northeastern$new_listing_count_yy, name = 'Northeastern', type='scatter', mode = 'lines') %>%
-  add_trace(y = northwestern$new_listing_count_yy, name = 'Northwestern', type='scatter', mode = 'lines') %>%
-  
+
+# Figure 10 - New listing count since 2019
+
+fig10 <- plot_ly(summed, x = ~month_date_yyyymm, y = ~new_listing_count_yy, color = ~REGION, type='scatter', mode = 'lines') %>%
   layout(
     title = "New Listings by Region During COVID-19 in New Jersey",
     yaxis = list(title = "Percent Change Since 2019",
@@ -150,17 +105,13 @@ fig10 <- plot_ly(coastal, x = ~month_date_yyyymm) %>%
     xaxis = list(
       type = "date",
       title = "Month",
-      range=c(min(coastal$month_date_yyyymm), max(coastal$month_date_yyyymm))
+      range=c(min(summed$month_date_yyyymm), max(summed$month_date_yyyymm))
     ))
 fig10
 
-fig11 <- plot_ly(coastal, x = ~month_date_yyyymm) %>% 
-  add_trace(y = ~new_listing_count_mm, name = 'Coastal', type='scatter', mode = 'lines') %>%
-  add_trace(y = central$new_listing_count_mm, name = 'Central', type='scatter', mode = 'lines') %>%
-  add_trace(y = southern$new_listing_count_mm, name = 'Southern', type='scatter', mode = 'lines') %>%
-  add_trace(y = northeastern$new_listing_count_mm, name = 'Northeastern', type='scatter', mode = 'lines') %>%
-  add_trace(y = northwestern$new_listing_count_mm, name = 'Northwestern', type='scatter', mode = 'lines') %>%
-  
+# Figure 11 - New listing count month-to-month
+
+fig11 <- plot_ly(summed, x = ~month_date_yyyymm, y = ~new_listing_count_mm, color = ~REGION, type='scatter', mode = 'lines') %>%
   layout(
     title = "New Listings by Region During COVID-19 in New Jersey",
     yaxis = list(title = "Percent Change Since Prior Month",
@@ -168,7 +119,7 @@ fig11 <- plot_ly(coastal, x = ~month_date_yyyymm) %>%
     xaxis = list(
       type = "date",
       title = "Month",
-      range=c(min(coastal$month_date_yyyymm), max(coastal$month_date_yyyymm))
+      range=c(min(summed$month_date_yyyymm), max(summed$month_date_yyyymm))
     ))
 fig11
 
